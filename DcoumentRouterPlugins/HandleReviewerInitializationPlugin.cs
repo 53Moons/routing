a3e23a2536417c356cae684a366d6318a68c4258
Blake Bradford <james.b.bradford@faa.gov>
2026-02-10 14:01:10 -0500

    Add HandleReviewerInitializationPlugin

---
+++ DcoumentRouterPlugins/HandleReviewerInitializationPlugin.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Xrm.Sdk;
+
+namespace DcoumentRouterPlugins
+{
+    public class HandleReviewerInitializationPlugin : PluginBase
+    {
+        public HandleReviewerInitializationPlugin() 
+            : base(typeof(HandleReviewerInitializationPlugin)) 
+        { 
+            // Not Implemented
+        }
+
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            var context = localPluginContext.PluginExecutionContext;
+            var sysService = localPluginContext.SystemUserService;
+            var tracer = localPluginContext.TracingService;
+        }
+    }
+}
