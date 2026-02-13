960b5e4a0417723a8368f0cd143899e495b55e73
Blake Bradford <james.b.bradford@faa.gov>
2026-02-13 08:35:04 -0500

    Refactor plugin to focus on parallel routing logic
    Removed unused usings, status/type checks, and serial routing logic. Consolidated execution into ExecuteCdsPlugin and deleted example/query methods, streamlining the plugin for parallel routing scenarios only.

--- DcoumentRouterPlugins/HandleReviewerInitializationPlugin.cs
+++ DcoumentRouterPlugins/HandleReviewerInitializationPlugin.cs
@@ -2,15 +2,6 @@
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Query;
 using System;
-﻿
-using Microsoft.Crm.Sdk.Messages;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Extensions;
-using Microsoft.Xrm.Sdk.Query;
-using System;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml;
 
 namespace DcoumentRouterPlugins
 {
@@ -26,17 +17,6 @@ namespace DcoumentRouterPlugins
             : base(typeof(HandleReviewerInitializationPlugin))
         {
             // Not Implemented
-        }
-        public new void Execute(IServiceProvider serviceProvider)
-        {
-            IPluginExecutionContext context =
-                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-
-            IOrganizationServiceFactory serviceFactory =
-            (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-
-            
-            serviceFactory.CreateOrganizationService(context.UserId);
         }
 
         protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
@@ -130,39 +110,6 @@ namespace DcoumentRouterPlugins
                 }
 
                 #endregion
-            //trigger when status goes from Draft to Pending Review
-            if (context.MessageName != "Update" || context.PrimaryEntityName != "cr8d2_routingsummary")
-            {
-                return;
-            }
-            if (!context.InputParameters.Contains("Target"))
-            {
-                return;
-            }
-            Entity target = (Entity)context.InputParameters["Target"];
-            
-            if (!target.Contains("routingstatus"))
-            {
-                return;
-            }
-
-            Entity preImage = context.PreEntityImages["PreImage"];
-            Entity postImage = context.PostEntityImages["PostImage"];
-            
-
-            OptionSetValue preStatus = preImage.GetAttributeValue<OptionSetValue>("routingstatus");
-            OptionSetValue postStatus = postImage.GetAttributeValue<OptionSetValue>("routingstatus");
-
-            const int PendingReview = 905200001;
-
-            if (postStatus.Value != PendingReview)
-            {
-                return;
-            }
-            Guid parentId = postImage.Id;
-
-            // plugin registration is only on status column
-            // doesnt mean the status column changed
 
                 #region Handle Parallel
                 // if parallel, bulk create "Action items"
@@ -209,28 +156,7 @@ namespace DcoumentRouterPlugins
             {
                 tracer.Trace($"Unhandled exception: {ex.Message}");
                 throw new InvalidPluginExecutionException(ex.Message, ex);
-            OptionSetValue preStatusType = preImage.GetAttributeValue<OptionSetValue>("routingtype");
-            OptionSetValue postStatusType = postImage.GetAttributeValue<OptionSetValue>("routingtype");
-
-            const int RoutingType_Serial = 905200000;
-            const int RoutingType_Parallel = 905200001;
-
-            if (postStatusType.Value == RoutingType_Serial)
-            {
-                return;
             }
         }
-            public static EntityCollection SimpleExample(IOrganizationService service) {
-                QueryExpression query = new QueryExpression("cr8d2_documentrouterdecision");
-                query.ColumnSet = new ColumnSet();
-                query.ColumnSet.AddColumns("cr8d2_name", "cr8d2_order", "cr8d2_distributionstatus");
-                query.AddOrder("cr8d2_order", OrderType.Ascending);
-                query.Criteria.AddCondition("cr8d2_routingsummaryid", ConditionOperator.Equal, parentId);
-                return service.RetrieveMultiple(query);
-            }
-
-        // if parallel, bulk create "Action items"
-
-        // if serial, create first action item (order dependant)
     }
 }
