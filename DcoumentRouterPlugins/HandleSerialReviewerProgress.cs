8ca0ce2a6c4f728bd90ac929ef9f7056c6ff31e3
Blake Bradford <james.b.bradford@faa.gov>
2026-02-25 16:27:45 -0500

    Update to handle reviewer ownership

--- DcoumentRouterPlugins/HandleSerialReviewerProgress.cs
+++ DcoumentRouterPlugins/HandleSerialReviewerProgress.cs
@@ -16,12 +16,12 @@ namespace DcoumentRouterPlugins
 
         // Routing Status OptionSet Value
         private const int ReviewComplete = 905000002;
-        private const string RoutStatus = "cr8d2_routingstatus"; 
+        private const string RoutStatus = "cr8d2_routingstatus";
 
         // Workflow Status OptionSet Value
         private const int PendingInitiatorAction = 905200012;
         private const int SerialReviewPending = 905200003;
-        private const string FlowStatus = "cr8d2_workflowstatus"; 
+        private const string FlowStatus = "cr8d2_workflowstatus";
 
         // Handle Reject Response
         private const int RejectedByReviewer = 905200006;
@@ -30,6 +30,7 @@ namespace DcoumentRouterPlugins
         // Entity References
         private const string ParentEntityName = "cr8d2_routingsummary";
         private const string ChildEntityName = "cr8d2_documentrouterdecision";
+        private const string ReviewerLookup = "cr8d2_distributionname";
 
         // Handle Order
         private const string ParentId = "cr8d2_routingsummaryid";
@@ -48,37 +49,50 @@ namespace DcoumentRouterPlugins
             var tracer = localPluginContext.TracingService;
 
             tracer.Trace("StartSerialReviewerProgress");
-                        
+
             // Check stage is post operation 40
             if (context.MessageName != "Update" || context.Stage != 40)
                 return;
 
             try
             {
-              
                 if (!context.PostEntityImages.TryGetValue("Image", out Entity postImage))
                     throw new Exception("Post Image is required.");
                 if (!context.PreEntityImages.TryGetValue("Image", out Entity preImage))
                     throw new Exception("Pre Image is required.");
 
                 // Confirm distribution status in image
-                if (!postImage.TryGetAttributeValue(DistStatus, out OptionSetValue postDistributionStatus))
+                if (
+                    !postImage.TryGetAttributeValue(
+                        DistStatus,
+                        out OptionSetValue postDistributionStatus
+                    )
+                )
                     throw new Exception("Distribution Status not in Post Image");
-                if (!preImage.TryGetAttributeValue(DistStatus, out OptionSetValue preDistributionStatus))
+                if (
+                    !preImage.TryGetAttributeValue(
+                        DistStatus,
+                        out OptionSetValue preDistributionStatus
+                    )
+                )
                     throw new Exception("Distribution Status not in Pre Image");
 
                 // Check Workflow Status serial review pending (triggered from initial flow)
-                OptionSetValue preWorkflowStatus = preImage.GetAttributeValue<OptionSetValue>(FlowStatus);
-                OptionSetValue postWorkflowStatus = postImage.GetAttributeValue<OptionSetValue>(FlowStatus);
+                OptionSetValue preWorkflowStatus = preImage.GetAttributeValue<OptionSetValue>(
+                    FlowStatus
+                );
+                OptionSetValue postWorkflowStatus = postImage.GetAttributeValue<OptionSetValue>(
+                    FlowStatus
+                );
 
-                bool isSerialReview = (preWorkflowStatus != null && preWorkflowStatus.Value == SerialReviewPending) ||
-                                      (postWorkflowStatus != null && postWorkflowStatus.Value == SerialReviewPending);
+                //bool isSerialReview = (preWorkflowStatus != null && preWorkflowStatus.Value == SerialReviewPending) ||
+                //                      (postWorkflowStatus != null && postWorkflowStatus.Value == SerialReviewPending);
 
-                if (!isSerialReview)
-                {
-                    tracer.Trace("Workflow status is not SerialReviewPending. Exiting to allow other routing types to process.");
-                    return;
-                }
+                //if (!isSerialReview)
+                //{
+                //    tracer.Trace("Workflow status is not SerialReviewPending. Exiting to allow other routing types to process.");
+                //    return;
+                //}
 
                 // Distribution status has to be pending
                 if (preDistributionStatus.Value != IsPending)
@@ -88,25 +102,41 @@ namespace DcoumentRouterPlugins
                 }
 
                 // Verify completed or rejected
-                if (postDistributionStatus.Value != Complete && postDistributionStatus.Value != Rejected)
+                if (
+                    postDistributionStatus.Value != Complete
+                    && postDistributionStatus.Value != Rejected
+                )
                 {
-                    tracer.Trace($"Distribution status changed to {postDistributionStatus.Value}, which is neither Complete nor Rejected. Exiting.");
+                    tracer.Trace(
+                        $"Distribution status changed to {postDistributionStatus.Value}, which is neither Complete nor Rejected. Exiting."
+                    );
                     return;
                 }
 
-                // Get parent 
+                // Get parent
                 var parentReference = postImage.GetAttributeValue<EntityReference>(ParentId);
                 if (parentReference == null)
                 {
-                    throw new Exception($"Parent routing summary lookup ({ParentId}) missing from distribution.");
+                    throw new Exception(
+                        $"Parent routing summary lookup ({ParentId}) missing from distribution."
+                    );
                 }
+                var parentEntity = sysService.Retrieve(
+                    ParentEntityName,
+                    parentReference.Id,
+                    new ColumnSet("ownerid")
+                );
 
                 // If rejected
                 if (postDistributionStatus.Value == Rejected)
                 {
+                    // TODO: Should this set the status of remaining reviewers as well?
                     tracer.Trace("Reviewer Rejected. Terminating Workflow.");
 
-                    Entity parentUpdate = new Entity(ParentEntityName, parentReference.Id);
+                    Entity parentUpdate = new Entity(
+                        parentReference.LogicalName,
+                        parentReference.Id
+                    );
                     parentUpdate[FlowStatus] = new OptionSetValue(WorkflowTerminated);
                     parentUpdate[RoutStatus] = new OptionSetValue(RejectedByReviewer);
 
@@ -119,7 +149,7 @@ namespace DcoumentRouterPlugins
                 {
                     tracer.Trace("Reviewer Completed. Finding next reviewer.");
 
-                    // Get next reviewer 
+                    // Get next reviewer
                     QueryExpression queryNextReviewer = new QueryExpression(ChildEntityName)
                     {
                         ColumnSet = new ColumnSet(DistStatus),
@@ -127,8 +157,16 @@ namespace DcoumentRouterPlugins
                         {
                             Conditions =
                             {
-                                new ConditionExpression(ParentId, ConditionOperator.Equal, parentReference.Id),
-                                new ConditionExpression(DistStatus, ConditionOperator.Equal, NotStarted)
+                                new ConditionExpression(
+                                    ParentId,
+                                    ConditionOperator.Equal,
+                                    parentReference.Id
+                                ),
+                                new ConditionExpression(
+                                    DistStatus,
+                                    ConditionOperator.Equal,
+                                    NotStarted
+                                )
                             }
                         }
                     };
@@ -141,11 +179,15 @@ namespace DcoumentRouterPlugins
                     // Reviewer assigned, reviewer finishes, count starts at 0 each iteration
                     if (nextReviewers.Entities.Count > 0)
                     {
-                        // Set next reviewer to IsPending
+                        // Set next reviewer to IsPending and transfer ownership
                         Entity nextReviewer = nextReviewers.Entities[0];
                         Entity updateReviewer = new Entity(ChildEntityName, nextReviewer.Id);
 
                         updateReviewer[DistStatus] = new OptionSetValue(IsPending);
+                        updateReviewer["ownerid"] = nextReviewer.GetAttributeValue<EntityReference>(
+                            ReviewerLookup
+                        );
+
                         sysService.Update(updateReviewer);
 
                         tracer.Trace("Next reviewer updated to IsPending.");
@@ -161,6 +203,15 @@ namespace DcoumentRouterPlugins
 
                         sysService.Update(parentUpdate);
                     }
+
+                    // Transfer ownership to parent owner no matter what
+                    sysService.Update(
+                        new Entity(postImage.LogicalName, postImage.Id)
+                        {
+                            ["ownerId"] = parentEntity.GetAttributeValue<EntityReference>("ownerid")
+                        }
+                    );
+                    tracer.Trace("Reviewer ownership transferred to parent owner.");
                 }
             }
             catch (Exception ex)
