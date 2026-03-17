e975f16394d1396e0e8fd92f5df19ddc524849cc
PEH <phammond16@gmail.com>
2026-03-17 09:36:17 -0700

    Refactor serial review progress to set action with and next action

--- DcoumentRouterPlugins/HandleSerialReviewerProgress.cs
+++ DcoumentRouterPlugins/HandleSerialReviewerProgress.cs
@@ -15,11 +15,11 @@ namespace DcoumentRouterPlugins
         private const string DistStatus = "cr8d2_distributionstatus";
 
         // Routing Status OptionSet Value
-        private const int ReviewComplete = 905200002;        
+        private const int ReviewComplete = 905200002;
         private const string RoutStatus = "cr8d2_routingstatus";
 
         // Routing Type OptionSet Value
-        private const int Serial = 905200000;       
+        private const int Serial = 905200000;
         private const string RoutType = "cr8d2_routingtype";
 
         // Workflow Status OptionSet Value
@@ -32,11 +32,21 @@ namespace DcoumentRouterPlugins
 
         // Entity References
         private const string ParentEntityName = "cr8d2_routingsummary";
-        private const string ChildEntityName = "cr8d2_documentrouterdecision";        
+        private const string ChildEntityName = "cr8d2_documentrouterdecision";     
 
         // Handle Order
         private const string ParentId = "cr8d2_routingsummary";
         private const string SetOrder = "cr8d2_order";
+
+        // Routing summary fields to set actionwith and actionnext
+        private const string ActionWith = "cr8d2_actionwith";
+        private const string ActionNext = "cr8d2_actionnext";
+
+        // Reviewer Approver lookup fields
+        private const string ReviewerLookup = "cr8d2_distributionname";      
+
+        // Owner Email
+        private const string OwnerEmail = "cr8d2_owneremail";
 
         public HandleSerialReviewerProgressPlugin()
             : base(typeof(HandleSerialReviewerProgressPlugin))
@@ -91,8 +101,8 @@ namespace DcoumentRouterPlugins
                     throw new Exception($"Parent routing summary lookup ({ParentId}) missing from reviewer distribution.");
                 }
 
-                // Check routing type is serial
-                Entity parent = sysService.Retrieve(ParentEntityName, parentReference.Id, new ColumnSet(RoutType));
+                // Check routing type is serial and get owner email  
+                Entity parent = sysService.Retrieve(ParentEntityName, parentReference.Id, new ColumnSet(RoutType, OwnerEmail));
                 if (!parent.Contains(RoutType) || parent.GetAttributeValue<OptionSetValue>(RoutType).Value != Serial)
 
                 {
@@ -108,6 +118,8 @@ namespace DcoumentRouterPlugins
                     Entity parentUpdate = new Entity(ParentEntityName, parentReference.Id);
                     parentUpdate[FlowStatus] = new OptionSetValue(WorkflowTerminated);
                     parentUpdate[RoutStatus] = new OptionSetValue(RejectedByReviewer);
+                    parentUpdate[ActionWith] = "None";
+                    parentUpdate[ActionNext] = "None";
 
                     sysService.Update(parentUpdate);
                     return;
@@ -118,10 +130,10 @@ namespace DcoumentRouterPlugins
                 {
                     tracer.Trace("Reviewer Completed. Finding next reviewer.");
 
-                    // Get next reviewer 
+                    // Get next reviewer - updated to pull 2 for action with and action next
                     QueryExpression queryNextReviewer = new QueryExpression(ChildEntityName)
                     {
-                        ColumnSet = new ColumnSet(DistStatus),
+                        ColumnSet = new ColumnSet(DistStatus, ReviewerLookup),
                         Criteria = new FilterExpression
                         {
                             Conditions =
@@ -134,9 +146,10 @@ namespace DcoumentRouterPlugins
 
                     // Get next reviewer order
                     queryNextReviewer.AddOrder(SetOrder, OrderType.Ascending);
-                    queryNextReviewer.TopCount = 1;
+                    queryNextReviewer.TopCount = 2;
 
                     EntityCollection nextReviewers = sysService.RetrieveMultiple(queryNextReviewer);
+
                     // Reviewer assigned, reviewer finishes, count starts at 0 each iteration
                     if (nextReviewers.Entities.Count > 0)
                     {
@@ -148,29 +161,62 @@ namespace DcoumentRouterPlugins
                         sysService.Update(updateReviewer);
 
                         tracer.Trace("Next reviewer updated to IsPending.");
+
+                        // Set current reviewer as action with and next reviewer (if exists) as action next
+                        EntityReference nextReviewerRef = nextReviewer.GetAttributeValue<EntityReference>(ReviewerLookup);
+                        string actionWithName = null;
+                        if (nextReviewerRef != null)
+                        {
+                            actionWithName = nextReviewerRef.Name;
+                        }
+                        string actionNextName = null;
+                        if (nextReviewers.Entities.Count > 1)
+                        {
+                            EntityReference secondReviewerRef = nextReviewers.Entities[1].GetAttributeValue<EntityReference>(ReviewerLookup);
+                            if (secondReviewerRef != null)
+                            {
+                                actionNextName = secondReviewerRef.Name;
+                            }
+                            tracer.Trace("Second reviewer found for action next.");
+                        }
+                        else
+                        {
+                            tracer.Trace("Second reviewer not found");
+                        }
+
+                        Entity parentActionUpdate = new Entity(ParentEntityName, parentReference.Id);
+                        parentActionUpdate[ActionWith] = actionWithName;
+                        parentActionUpdate[ActionNext] = actionNextName;
+                        sysService.Update(parentActionUpdate);
+
+                        tracer.Trace("Parent routing summary updated with action with and action next.");
                     }
                     else
                     {
-                        // No additional reviewers found. Review is complete.
-                        tracer.Trace("No additional reviewers. Review complete");
+                        // No additional reviewers found. Review is complete
+                        tracer.Trace("No additional reviewers found. Review complete.");
+
+                        string ownerEmail = parent.GetAttributeValue<string>(OwnerEmail);
 
                         Entity parentUpdate = new Entity(ParentEntityName, parentReference.Id);
                         parentUpdate[RoutStatus] = new OptionSetValue(ReviewComplete);
                         parentUpdate[FlowStatus] = new OptionSetValue(PendingInitiatorAction);
+                        parentUpdate[ActionWith] = ownerEmail;
+                        parentUpdate[ActionNext] = "Pending";
 
                         sysService.Update(parentUpdate);
                     }
                 }
             }
+
             catch (Exception ex)
             {
                 tracer.Trace($"Error in HandleSerialReviewerProgressPlugin: {ex.Message}");
                 throw new InvalidPluginExecutionException(ex.Message, ex);
             }
+            }
 
-                   
 
-                
+
         }
     }
-}
